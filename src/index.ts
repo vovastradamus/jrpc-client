720341d61fae2b8df028802ed27b2a8a272645df
vovastradamus <vovastradamus@gmail.com>
2021-07-20 02:08:07 +0500

    index file exports

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,3 @@
-// import "./jrpc-client";
-import "./jrpc.client";
+export { JrpcClient } from "./jrpc.client";
+export { JrpcFetchProvider } from "./jrpc-fetch.provider";
+export { JrpcConsoleProvider } from "./jrpc-console.provider";
