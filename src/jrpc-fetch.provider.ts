c4516b7e8c69356f75e739a9032d39c3bb406ec6
vovastradamus <vovastradamus@gmail.com>
2021-07-22 01:47:28 +0500

    jrpc fetch init

--- src/jrpc-fetch.provider.ts
+++ src/jrpc-fetch.provider.ts
@@ -2,15 +2,44 @@ import { JrpcBaseAbstractProvider } from "./jrpc-base.provider";
 import { TProviderRequestBody } from "./jrpc.types";
 
 export class JrpcFetchProvider extends JrpcBaseAbstractProvider {
-  constructor(private baseUrl: string) {
+  private baseUrl: string;
+  private configMerge?: (config: RequestInit) => RequestInit;
+
+  constructor({
+    baseUrl,
+    configMerge,
+  }: {
+    baseUrl: string;
+    configMerge: <T>(config: T) => T;
+  }) {
     super();
+
+    this.baseUrl = baseUrl;
+    this.configMerge = configMerge;
   }
 
   async send(operations: TProviderRequestBody) {
-    const request = await fetch(this.baseUrl, {
+    const requestInit: RequestInit = {
       method: "POST",
-      body: JSON.stringify(operations),
-    });
+      mode: "cors",
+      cache: "no-cache",
+      credentials: "same-origin",
+      redirect: "follow",
+      referrerPolicy: "no-referrer",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    const request = await fetch(
+      this.baseUrl,
+      Object.assign(
+        this.configMerge ? this.configMerge(requestInit) : requestInit,
+        {
+          body: JSON.stringify(operations),
+        }
+      )
+    );
 
     return request.json();
   }
